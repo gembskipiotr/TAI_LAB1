82638f5094a1cc7b4124cd269ea80d7b9611b16b
pioogeb@gmail.com <pikejksr@gmail.com>
2019-04-11 20:56:17 +0200

    navbar component

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let nav = '<ul class="nav justify-content-center">' +
+    '<li class="nav-item"><a href="index.html">Home</a></li>' +
+    '<li class="nav-item"><a href="#">Blog</a></li>' +
+    '<li class="nav-item"><a href="#">Quiz</a></li>'+
+    '<li class="nav-item"><a href="contact.html">Kontakt</a></li>'+
+'</ul>';
+
+let elem = document.querySelector('.navbar');
+elem.innerHTML = nav;
